0e3bf49041f3653b4f0fcff29725b434d7b1c439
Anton Sobolev <2185430@gmail.com>
2020-08-02 19:48:49 +0300

    homework1

---
+++ first-web-app/src/main/java/ru/gekkbrains/FirstServlet.java
@@ -0,0 +1,35 @@
+package ru.gekkbrains;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class FirstServlet implements Servlet {
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+        this.config = config;
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return this.config;
+    }
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        res.getWriter().println("<h1> Hello from servlet</h1>");
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+    private transient ServletConfig config;
+
+
+}
